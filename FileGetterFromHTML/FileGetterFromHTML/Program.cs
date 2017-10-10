faff48a6231575cb472273a2f1dfa761b17d07c0
windmill mc <fujitsubo1324@gmail.com>
2017-10-11 03:08:01 +0900

    parse実装

--- FileGetterFromHTML/FileGetterFromHTML/Program.cs
+++ FileGetterFromHTML/FileGetterFromHTML/Program.cs
@@ -1,15 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
-namespace FileGetterFromHTML
+using System.IO;
+namespace Fileutil
 {
-	class Program
+	class FileGetterFromHTML
 	{
+		static readonly string url = "http://azurlane.wikiru.jp/index.php?%A5%AB%A5%C3%A5%B7%A5%F3";
 		static void Main(string[] args)
 		{
+			string inputedURL = url;
+			string gettedSource = HTMLSouceGetter.GetSource(inputedURL,Encoding.GetEncoding("EUC-JP"));
+			Console.Write(ParseLine(gettedSource,"jpg","attach2"));
+			Console.ReadKey();
+
+		}
+
+
+		static string ParseLine(string gettedSource,params string[]  search)
+		{
+			string currentLine = "";
+			StringReader stringReader = new StringReader(gettedSource);
+			while(currentLine!=null)
+			{
+				string[] parsed = currentLine.Split('"');
+				if(parsed.Length>0)
+				{
+					var s = parsed.Where(X=>X.Contains(search[0]) && X.Contains(search[1]));
+					if(s.Count()>0)
+						return s.First();
+
+				}
+				currentLine = stringReader.ReadLine();
+			}
+
+			return null;
 		}
 	}
 }
